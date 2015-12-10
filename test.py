2c873f611cbd017b50b378b2353f9fdb04335fe8
MuraseTomohiro <mrs.tmhr@gmail.com>
2015-12-10 11:49:37 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python
+
+import sys
+
+if len(sys.argv) <= 2:
+    print >> sys.stderr,"Useage: test <integer> <integer>"
+    sys.exit
+
+def add(a,b):
+    c = a+b
+    return c
+
+def mult(a,b):
+    c = a*b
+    return c
+
+if __name__ == "__main__":
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    if len(sys.argv) == 4:
+        opt = sys.argv[3]
+    else:
+        opt = 'default'
+    for i in range(1,x+1):
+        print "{0:3d}".format(i),'|',
+        for j in range(1,y+1):
+            if opt == 'default':
+                print "{0:3d}".format(mult(i,j)),
+            elif opt == '-a':
+                print "{0:3d}".format(add(i,j)),
+        print '\n',
+

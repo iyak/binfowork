c83a4a7c5deddeebc91e9dd73dc3bed0fabf0b50
MuraseTomohiro <mrs.tmhr@gmail.com>
2015-12-16 00:05:39 +0900

    remove garvages

--- build/c4che/_cache.py
+++
@@ -1,9 +0,0 @@
-BINDIR = '/usr/local/bin'
-LIBDIR = '/usr/local/lib'
-PREFIX = '/usr/local'
-PYC = 1
-PYCMD = '"import sys, py_compile;py_compile.compile(sys.argv[1], sys.argv[2])"'
-PYFLAGS = ''
-PYFLAGS_OPT = '-O'
-PYO = 1
-PYTHON = ['/usr/bin/python']
--- build/c4che/build.config.py
+++
@@ -1,2 +0,0 @@
-version = 0x1070f00
-tools = [{'tool': 'python', 'tooldir': None, 'funs': None}]

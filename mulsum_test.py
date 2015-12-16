e7bc14399ede0a719737464c7ad4cc035b083ecb
yoshiki <yoshiki@example.com>
2015-12-16 21:04:02 +0900

    add test

--- mulsum_test.py
+++ mulsum_test.py
@@ -8,10 +8,12 @@ class MulSumTest(unittest.TestCase):
         self.assertEqual(mulsum.add(1,2), 3)
         self.assertEqual(mulsum.add(3,4), 7)
         self.assertEqual(mulsum.add(5,6), 11)
+        self.assertEqual(mulsum.add(7,8), 15)
     def test_mult(self):
         self.assertEqual(mulsum.mult(1,2), 2)
         self.assertEqual(mulsum.mult(3,4), 12)
         self.assertEqual(mulsum.mult(5,6), 30)
+        self.assertEqual(mulsum.add(7,8), 40)
 
 if __name__ == '__main__':
     unittest.main()
